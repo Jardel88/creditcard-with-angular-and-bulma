8643b4f2400dc0ad6c43a87c234f68468d45660c
jardel88 <jardelsilvagomes@hotmail.com>
2023-04-07 17:22:26 -0300

    Resetting form

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -39,4 +39,8 @@ export class CardFormComponent {
     console.log('Form was submited');
   }
 
+  onResetClick() {
+    this.cardForm.reset();
+  }
+
 }
--- src/app/date-form-control.ts
+++ src/app/date-form-control.ts
@@ -2,7 +2,12 @@ import { FormControl } from '@angular/forms';
 
 export class DateFormControl extends FormControl{
 
-  override setValue(value: string, options: any) {
+  override setValue(value: string | null, options: any) {
+
+    if (!value) {
+      super.setValue('', { ...options, emitModelToViewChange: true });
+      return;
+  }
 
     if (value.match(/[^0-9|\/]/gi)) {
         super.setValue(this.value, { ...options, emitModelToViewChange: true });
@@ -15,11 +20,11 @@ export class DateFormControl extends FormControl{
     }
 
     if (value.length === 2 && this.value.length === 3) {
-      super.setValue( value, { ...options, emitModelToViewChange: true });
+      super.setValue(value, { ...options, emitModelToViewChange: true });
       return;
     }
 
-    if (value.length === 2) {
+    if (value!.length === 2) {
       super.setValue(value + '/', { ...options, emitModelToViewChange: true});
       return;
     }
